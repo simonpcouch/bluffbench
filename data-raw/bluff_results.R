2039217d7c57c2a0aecbae05a23e09cead512fcb
simonpcouch <simonpatrickcouch@gmail.com>
2025-10-08 16:20:53 -0500

    run against 3 models across 3 epochs

--- data-raw/bluff_results.R
+++ data-raw/bluff_results.R
@@ -5,6 +5,13 @@ bluff_results_raw <- process_results()
 bluff_results <-
   bluff_results_raw %>%
   rename(model = task) %>%
-  select(-metadata)
+  select(-metadata) %>%
+  mutate(
+    model = case_when(
+      model == "claude_4_5_sonnet" ~ "Claude Sonnet 4.5",
+      model == "gemini_2_5_pro" ~ "Gemini Pro 2.5",
+      model == "gpt_5" ~ "GPT-5"
+    )
+  )
 
 usethis::use_data(bluff_results, overwrite = TRUE)
